d328536804229465a52e2e21bf9ec030ba8d4f2e
Alexei Shpikat <a.shpikat@gmail.com>
2020-12-01 18:15:53 +0300

    Add Day 01, part 2 :star:

---
+++ day01/part2.py
@@ -0,0 +1,30 @@
+def main():
+    expenses = read_expenses()
+
+    expenses.sort()
+    for index_third, third in enumerate(expenses):
+        target = 2020 - third
+        little = 0
+        big = len(expenses) - 1
+        while little < big:
+            current = expenses[little] + expenses[big]
+            if current < target:
+                little += 1
+            elif current > target:
+                big -= 1
+            else:
+                print(expenses[little] * expenses[big] * third)
+                return
+    else:
+        print("No three items summing 2020 are detected")
+
+
+def read_expenses():
+    with open("input", "r") as file:
+        lines = file.readlines()
+    expenses = [int(line.strip()) for line in lines]
+    return expenses
+
+
+if __name__ == "__main__":
+    main()
