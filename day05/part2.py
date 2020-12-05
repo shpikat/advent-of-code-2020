4777f5f1a43e54bc0173a68e9b8cf275d606399d
Alexei Shpikat <a.shpikat@gmail.com>
2020-12-05 14:07:50 +0300

    Rewrite into index check for better O() :zap:
    Intermediate step to switch from O(N) to O(lgN)

--- day05/part2.py
+++ day05/part2.py
@@ -1,3 +1,6 @@
+import operator
+from typing import List
+
 from day05.common import read_boarding_tickets, code_to_id
 
 
@@ -7,17 +10,16 @@ def main():
     seats = [code_to_id(ticket) for ticket in boarding_tickets]
 
     seats.sort()
-    starting_seat = (seats[0] & 0b1111111000) + 0b1000
-    ending_seat = seats[-1] & 0b1111111000
-    # may as well be just 0, as the task says there are seats in front
-    previous_seat = starting_seat - 1
-    for seat in seats:
-        if starting_seat <= seat < ending_seat:
-            expected_seat = previous_seat + 1
-            if expected_seat != seat:
-                print(expected_seat)
-                break
-        previous_seat = seat
+    start_index = seats.index((seats[0] & 0b1111111000) + 0b1000)
+    end_index = rindex(seats, (seats[-1] & 0b1111111000) - 1)
+    for index, seat in enumerate(seats[start_index:end_index], seats[start_index]):
+        if index != seat:
+            print(index)
+            break
+
+
+def rindex(lst: List[int], value: int) -> int:
+    return len(lst) - operator.indexOf(reversed(lst), value) - 1
 
 
 if __name__ == "__main__":
