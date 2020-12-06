fbc15e32959530e322872ebddf4dd3b7c313401d
Alexei Shpikat <a.shpikat@gmail.com>
2020-12-06 15:56:21 +0300

    Rewrite using bitwise logic

--- day06/part1.py
+++ day06/part1.py
@@ -3,12 +3,16 @@ from day06.common import read_answers
 
 def main():
     answers = read_answers()
-    total_count = sum(
-        len(group_unique_answers) for group_unique_answers in
-        (set(single_answer
-             for person_answers in group_answers
-             for single_answer in person_answers)
-         for group_answers in answers))
+
+    total_count = 0
+    for group_answers in answers:
+        bitset = 0
+        for person_answers in group_answers:
+            for single_answer in person_answers:
+                bitset |= 1 << (ord(single_answer) - ord('a'))
+        # popcount operation the Python way
+        total_count += bin(bitset).count('1')
+
     print(total_count)
 
 
