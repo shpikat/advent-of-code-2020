16403281f2e5d138671ba9e078877ca24952001b
Alexei Shpikat <a.shpikat@gmail.com>
2021-10-16 23:26:48 +0300

    Add day 21, part 2 :star:

---
+++ day21/part2.py
@@ -0,0 +1,33 @@
+from day21.common import read_foods
+
+
+def main():
+    foods = read_foods()
+
+    candidates = {}
+    for food in foods:
+        for allergen in food.allergens:
+            if allergen in candidates:
+                candidates[allergen] &= set(food.ingredients)
+            else:
+                candidates[allergen] = set(food.ingredients)
+
+    allergens = {}
+    while candidates:
+        # need to have a hard copy as the dict is going to be modified
+        found = [allergen for allergen in candidates if len(candidates[allergen]) == 1]
+        ingredients_to_remove = set()
+        for allergen in found:
+            (ingredient,) = candidates[allergen]
+            allergens[allergen] = ingredient
+            ingredients_to_remove.add(ingredient)
+            del candidates[allergen]
+
+        for ingredients in candidates.values():
+            ingredients -= ingredients_to_remove
+
+    print(','.join(allergens[allergen] for allergen in sorted(allergens)))
+
+
+if __name__ == "__main__":
+    main()
